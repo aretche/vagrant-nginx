fb3f46f255850e4dc1eb954437a29fccc81be616
Gabriel Arellano <arellanog@cent.gov.ar>
2016-04-12 23:17:43 -0300

    Agregamos configuración para nginx y una página html de prueba

--- provision/setup.sh
+++ provision/setup.sh
@@ -2,11 +2,17 @@
 
 echo "Provisioning virtual machine..."
 
-echo "Actualizando lista de paquetes"
+echo "Actualizando lista de paquetes.."
 apt-get update
 
-echo "Instalando Git"
+echo "Instalando Git.."
 apt-get install git -y 
     
-echo "Instalando Nginx"
+echo "Instalando Nginx.."
 apt-get install nginx -y 
+
+echo "Configurando Nginx.."
+cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
+ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
+rm -rf /etc/nginx/sites-available/default
+service nginx restart
