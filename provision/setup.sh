7f9dc9d42cf629c5be146dc2ea96baa6a1315bdd
Gabriel Arellano <arellanog@cent.gov.ar>
2016-04-12 23:04:38 -0300

    Agregamos a setup.sh las instrucciones para instalar git y nginx

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Provisioning virtual machine..."
+
+echo "Actualizando lista de paquetes"
+apt-get update
+
+echo "Instalando Git"
+apt-get install git -y 
+    
+echo "Instalando Nginx"
+apt-get install nginx -y 
