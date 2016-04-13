d55c84bfeb6209e37681610107e018c1db225474
Gabriel Arellano <arellanog@cent.gov.ar>
2016-04-12 23:25:11 -0300

    Agregamos PHP 5 y las extensiones PHP

--- provision/setup.sh
+++ provision/setup.sh
@@ -16,3 +16,10 @@ cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost 
 ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-available/default
 service nginx restart
+
+echo "Instalando PHP..."
+apt-get install php5-common php5-cli php5-fpm -y
+    
+echo "Installing extensiones PHP..."
+apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y
+service nginx restart
