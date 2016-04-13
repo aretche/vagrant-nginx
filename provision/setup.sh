75470c650fadcaf5b258a9b2530deeb5de17926d
Gabriel Arellano <arellanog@cent.gov.ar>
2016-04-12 23:32:10 -0300

    Agregamos la instalación de MySQL (usuario:password root:root)

--- provision/setup.sh
+++ provision/setup.sh
@@ -20,6 +20,15 @@ service nginx restart
 echo "Instalando PHP..."
 apt-get install php5-common php5-cli php5-fpm -y
     
-echo "Installing extensiones PHP..."
+echo "Instalando extensiones PHP..."
 apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y
 service nginx restart
+
+echo "Instalando MySQL..."
+apt-get install debconf-utils -y 
+debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+apt-get install mysql-server -y
+
+
+
